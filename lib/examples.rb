94f5eff32a2142f5190351f7780d6841365e8deb
Isaac Cohen <icohen@leapmotion.com>
2013-09-11 19:01:07 -0700

    adding examples.rb

---
+++ lib/examples.rb
@@ -0,0 +1,36 @@
+
+EXAMPLES = [
+
+  [ 
+    "/examples/dom-visualizer.html",          # Link
+    "/images/examples/dom-visualizer.png",    # Image
+    "Hand and Finger 3D Visualizer",          # Title
+    "DOM based visualizer of data",           # Description
+    "@juliangarnier",                         # Author
+    "https://github.com/juliangarnier",       # Author Link
+    [                                         # Tags
+      "CSS3D",
+      "DOM",
+      "Visualizer",
+      "Dev Tool",
+      "LIVE!"
+    ]
+  ],
+
+  [
+    "/examples/earthRoation/index.html",
+    "/images/examples/earth.png",
+    "3D Globe",
+    "3D Spinnable Globe",
+    "@rtilton1",
+    "https://github.com/rtilton1",
+    [
+      "Three.js",
+      "WebGL",
+      "Data-Viz",
+      "LIVE!"
+    ]
+  ]
+
+
+]
