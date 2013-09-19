2081ab1797898a6b09256afacd2f8ee454d935b0
Josh Hull <joshbuddy@gmail.com>
2013-09-19 15:56:14 -0700

    update docs to new version

--- lib/versions.rb
+++ lib/versions.rb
@@ -1,2 +1,2 @@
-DOC_VERSIONS = ['v0.2.0', 'v0.2.0-beta6', 'v0.2.0-beta5', 'v0.2.0-beta4', 'v0.2.0-beta3', 'v0.2.0-beta2', 'v0.2.0-beta1', 'master']
-VISIBLE_DOC_VERSIONS = ['v0.2.0', 'master']
+DOC_VERSIONS = ['v0.2.1', 'v0.2.0', 'v0.2.0-beta6', 'v0.2.0-beta5', 'v0.2.0-beta4', 'v0.2.0-beta3', 'v0.2.0-beta2', 'v0.2.0-beta1', 'master']
+VISIBLE_DOC_VERSIONS = ['v0.2.1', 'master']
